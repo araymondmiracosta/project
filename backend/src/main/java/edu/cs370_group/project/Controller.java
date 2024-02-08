307156fc66094949cd0b979d71e2b386794c7db3
araymondmiracosta <araymond+miracostagithub@nebulacentre.net>
2024-02-08 09:57:05 -0800

    Add POST endpoint documentation

--- backend/src/main/java/edu/cs370_group/project/Controller.java
+++ backend/src/main/java/edu/cs370_group/project/Controller.java
@@ -3,6 +3,8 @@ package edu.cs370_group.project;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +14,11 @@ public class Controller {
 	public ResponseEntity<Object> test() {
 		Person newPerson = new Person(5, 2, "John");
 		return new ResponseEntity<>(newPerson.getMap(), HttpStatus.OK);
+	}
+
+//	@PostMapping("/sessions/{id}")
+	@GetMapping("/sessions/{id}")
+	public String postTest(@PathVariable String id) {
+		return ("{\"sessionId\":\"" + id + "\"}");
 	}
 }
